8f0610c567ec67918a5fb68ee93bd9512c7fdd8b
Charity Njeri <wekesacharity@gmail.com>
2026-02-26 10:22:26 +0300

    add MilkProduction admin registration

--- apps/milking/admin.py
+++ apps/milking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import MilkProduction
 
-# Register your models here.
+
+@admin.register(MilkProduction)
+class MilkProductionAdmin(admin.ModelAdmin):
+    list_display = ("cattle", "liters", "date_time", "created_at")
+    list_filter = ("date_time", "cattle__farm")
+    search_fields = ("cattle__tag_number",)
+    ordering = ("-date_time",)
